c96092c6d5fbfa70205b7c1392d0df80d54b3459
vincent <vclaes1986@gmail.com>
2017-04-20 21:02:48 +0000

    clean up ltsm script

--- body_weight_predictor/etl.py
+++ body_weight_predictor/etl.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 class ETL(object):
-    def __init__(self, file_path, start_of_predicition, dependend_variable_period):
+    def __init__(self, file_path, start_of_predicition, dependend_variable_period, show_plot=False):
         self.file_path = file_path  # 'bw_ross_308.csv'
         self.start_of_prediction = start_of_predicition
         self.dependend_variable_period = dependend_variable_period
@@ -12,56 +12,58 @@ class ETL(object):
         self.X_test = None
         self.y_train = None
         self.y_test = None
+        self.show_plot = show_plot
 
     def process(self):
         self.extract()
         self.transform()
         self.load()
+        self.plot()
         return self.X_train, self.X_test, self.y_train, self.y_test
 
     def extract(self):
         # READ IN DATA
         # -----------------------
-        self.df_raw = pd.read_csv(self.file_path).sort('age_days').reset_index(drop=True)
+        self.df_raw = pd.read_csv(self.file_path).sort_values('age_days').reset_index(drop=True)
         self.df_raw.head()
         return self.df_raw
 
     def transform(self):
         # DATA PREPROCESSING
         # -----------------------
-        print self.df_raw.shape
+        print(self.df_raw.shape)
         # remove empty columns
         self.df_raw = self.df_raw.dropna(axis=1, how='all')
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         # we can have only 1 value per day for bw.
         self.df_raw = self.df_raw.drop_duplicates(subset='age_days')
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         # remove data after 35 days
         self.df_raw = self.df_raw[self.df_raw['age_days'] <= self.dependend_variable_period]
         self.df_raw = self.df_raw.set_index('age_days', drop=True)
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         # remove rounds with no initial data
         mask_no_initial_data = self.df_raw.fillna(method='ffill').notnull()
         self.df_raw = self.df_raw.loc[:, mask_no_initial_data.all(axis=0)]
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         # remove rounds which have NaN before 35 days
         mask_no_end_data = self.df_raw.fillna(method='backfill').notnull()
         self.df_raw = self.df_raw.loc[:, mask_no_end_data.all(axis=0)]
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         # remove all columns that have same value
         self.df_raw = self.df_raw.loc[:, self.df_raw.all(axis=0)]
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         def duplicate_values_in_series(df, percentage_same_values=0.25):
             '''
@@ -85,24 +87,22 @@ class ETL(object):
 
         self.df_raw = duplicate_values_in_series(self.df_raw)
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
         # drop columns which have extreme values in the beginning of the round to nan.
         # sometimes the regulator is not reset before the next flock starts.
         # we ignore these extreme values
 
-        print self.df_raw.shape
+        print(self.df_raw.shape)
         # we keep the columns which have no extreme values in the first 5 days.
         self.df_raw = self.df_raw.loc[:, ~((self.df_raw.iloc[:5, :] > 500).any())]
-        print self.df_raw.shape
+        print(self.df_raw.shape)
 
-        self.df_raw.plot()
-        plt.show()
 
     def load(self):
         self.df_raw = self.df_raw.T
 
-        print self.df_raw.describe()
+        print(self.df_raw.describe())
 
         # get X, Y
 
@@ -115,3 +115,7 @@ class ETL(object):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=0)
         return self.X_train, self.X_test, self.y_train, self.y_test
 
+    def plot(self):
+        if self.show_plot:
+            self.df_raw.plot()
+            plt.show()
--- body_weight_predictor/long_short_term_memory_network_predictor.py
+++ body_weight_predictor/long_short_term_memory_network_predictor.py
@@ -3,9 +3,10 @@ import numpy as np
 import pandas as pd
 from body_weight_predictor.etl import ETL
 
-X_train, X_test, y_train, y_test = ETL('bw_ross_308.csv', 5, 35).process()
+etl = ETL('bw_ross_308.csv', 30, 35)
+X_train, X_test, y_train, y_test = etl.process()
 
-
+df_raw = etl.df_raw
 
 ########################################
 #
@@ -22,88 +23,66 @@ from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 
-
-# convert an array of values into a dataset matrix
-def create_dataset(dataset, look_back=1):
-    dataX, dataY = [], []
-    for i in range(len(dataset) - look_back - 1):
-        a = dataset[i:(i + look_back), 0]
-        dataX.append(a)
-        dataY.append(dataset[i + look_back, 0])
-    return np.array(dataX), np.array(dataY)
-
-
 # fix random seed for reproducibility
 np.random.seed(7)
-# load the dataset
-# dataframe = pd.read_csv('international-airline-passengers.csv', sep=';', usecols=[1], engine='python', skipfooter=3)
-# dataset = dataframe.values
-# dataset = dataset.astype('float32')
-# normalize the dataset
 
 # handle inf and nan values. if we normalize the values they cannot handle nan and inf
+# replace inf by nan
 df_raw = df_raw.replace([np.inf, -np.inf], np.nan)
 for column in df_raw:
+    # replace nan with mean of the column
     df_raw[column] = df_raw[column].fillna(df_raw[column].mean())
+
+# we need this dtype to normalize our data between 0 and 1
+# more on minmax scaling vs standardization
+# http://sebastianraschka.com/Articles/2014_about_feature_scaling.html#about-min-max-scaling
 df_raw = df_raw.astype('float32')
-# scaler = MinMaxScaler(feature_range=(0, 1))
-# df_raw = scaler.fit_transform(df_raw)
-# split into train and test sets
-# train_size = int(len(dataset) * 0.67)
-# test_size = len(dataset) - train_size
-# train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
-# reshape into X=t and Y=t+1
-X = df_raw.iloc[:,0:5]
+X = df_raw.iloc[:, 0:5]
 x_scaler = MinMaxScaler(feature_range=(0, 1))
 X = x_scaler.fit_transform(X)
 
-y = df_raw.iloc[:,-1]
+y = df_raw.iloc[:, -1]
 y_scaler = MinMaxScaler(feature_range=(0, 1))
 y = y_scaler.fit_transform(y)
 
-dataset = df_raw
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-
 trainX, testX, trainY, testY = train_test_split(X, y, test_size=0.2, random_state=0)
 
-
 look_back = 5
-# trainX, trainY = create_dataset(train, look_back)
-# testX, testY = create_dataset(test, look_back)
+
 # reshape input to be [samples, time steps, features]
 trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
 testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
 # create and fit the LSTM network
+# http://deeplearning.net/tutorial/lstm.html
 model = Sequential()
-model.add(LSTM(4, input_dim=look_back))
+model.add(LSTM(4, input_shape=(None, look_back)))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam')
-model.fit(trainX, trainY, nb_epoch=10, batch_size=1, verbose=2)
+model.fit(trainX, trainY, epochs=10, batch_size=1, verbose=2)
+
 # make predictions
 trainPredict = model.predict(trainX)
 testPredict = model.predict(testX)
+
 # invert predictions
 trainPredict = y_scaler.inverse_transform(trainPredict)
 trainY = y_scaler.inverse_transform([trainY])
 testPredict = y_scaler.inverse_transform(testPredict)
 testY = y_scaler.inverse_transform([testY])
+
 # calculate root mean squared error
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
 print('Test Score: %.2f RMSE' % (testScore))
-# shift train predictions for plotting
-trainPredictPlot = np.empty_like(dataset)
-trainPredictPlot[:, :] = np.nan
-trainPredictPlot[look_back:len(trainPredict) + look_back, :] = trainPredict
-# shift test predictions for plotting
-testPredictPlot = np.empty_like(dataset)
-testPredictPlot[:, :] = np.nan
-testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - 1, :] = testPredict
-# plot baseline and predictions
-#fix this scaler
-# plt.plot(scaler.inverse_transform(dataset))
-plt.plot(trainPredictPlot)
-plt.plot(testPredictPlot)
+
+# create graph for test data set
+plt.suptitle('Test Dataset Pred. vs Obs.', fontsize=20)
+plt.plot(testPredict[:, 0], label='prediction')
+plt.plot(testY[0], label='observations')
+plt.ylabel('weight')
+plt.xlabel('samples')
+plt.legend(loc='upper right')
 plt.show()
